5c66c248bb725b3d02a8251195c15e75f3c40ed0
Satana145 <76929429+Satana145@users.noreply.github.com>
2022-05-18 16:51:27 -0700

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -13,30 +13,7 @@ function preload(){
 function setup(){
     canvas = createCanvas( 380 , 380);
     canvas.center();
-    
-    //initializing cocossd model
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML  =  "Status : Detecting Objects";
 }
-
-function modelLoaded(){
-    console.log("Model is loaded");
-     status = true;
-
-   objectDetector.detect(img , gotResults);
-}
-
-function gotResults(error , results){
-    if (error) {
-       console.log(error);
-
-    } 
-    else {
-       console.log(results);
-       objects = results;
-    }
-}
-
 function draw(){
     image(img , 0 , 0 , 380 , 380);
     fill("#FF0000");
